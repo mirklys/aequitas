9655bf965cf0491f107029dcaeb2e99a569b9061
mirklys <giedriusmirklys8@gmail.com>
2024-08-27 14:24:27 +0200

    total money

--- pages/home.py
+++ pages/home.py
@@ -15,3 +15,13 @@ class Home:
             self.aequitas.update_table(uploaded_file)
             st.write(f"Table {self.aequitas.table.table_name} updated")
             st.write(self.aequitas.get_data().head(5))
+        
+        # amount of incoming money and outgoing money
+        incoming = self.aequitas.get_data().loc[self.aequitas.get_data()["incoming"] == 1]
+        outgoing = self.aequitas.get_data().loc[self.aequitas.get_data()["incoming"] == 0]
+
+        st.write(f"Total incoming: {incoming['amount'].sum().round(2)} EUR")
+        st.write(f"Total outgoing: {outgoing['amount'].sum().round(2)} EUR")
+        st.write(f"Total balance: {(incoming['amount'].sum() - outgoing['amount'].sum()).round(2)} EUR")
+
+        
